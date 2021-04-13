0cb22ec9d602b56af2d34d7fb370d2fc22b73af8
Jacek <ja_cek@gmx.com>
2021-04-13 13:10:05 +0200

    project update

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" reequired placeholder="email" v-model="email">
+    <input type="password" reequired placeholder="password" v-model="password">
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = () => {
+      console.log(email.value, password.value)
+    }
+
+    return { email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" reequired placeholder="display name" v-model="displayName">
+    <input type="email" reequired placeholder="email" v-model="email">
+    <input type="password" reequired placeholder="password" v-model="password">
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const displayName = ref('')
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = () => {
+      console.log(displayName.value, email.value, password.value)
+    }
+
+    return { displayName, email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+
+</style>
