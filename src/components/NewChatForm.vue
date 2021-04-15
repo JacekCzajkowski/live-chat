0698f80e69575f57087d9f16c88911526c788df5
Jacek <ja_cek@gmx.com>
2021-04-15 16:57:23 +0200

    project update

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <textarea 
+      placeholder="Type a message and hit enter to send..."
+      v-model="message"
+    ></textarea>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import getUser from '../composables/getUser'
+import { timestamp } from '../firebase/config'
+
+export default {
+  setup() {
+    const { user } = getUser()
+    const message = ref('')
+
+    const handleSubmit = async () => {
+      const chat = {
+        name: user.value.displayName,
+        message: message.value,
+        createdAt: timestamp()
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
