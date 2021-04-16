ab595d5309592f8d3aee463d195f4894e8f9d33d
Jacek <ja_cek@gmx.com>
2021-04-16 11:07:19 +0200

    project update

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -3,18 +3,23 @@
     <textarea 
       placeholder="Type a message and hit enter to send..."
       v-model="message"
+      @keypress.enter.prevent="handleSubmit"
     ></textarea>
+    <div class="error">{{ error }}</div>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue'
 import getUser from '../composables/getUser'
+import useCollection from '../composables/useCollection'
 import { timestamp } from '../firebase/config'
 
 export default {
   setup() {
     const { user } = getUser()
+    const { addDoc, error } = useCollection('messages')
+
     const message = ref('')
 
     const handleSubmit = async () => {
@@ -23,11 +28,32 @@ export default {
         message: message.value,
         createdAt: timestamp()
       }
+    
+      await addDoc(chat)
+      if (!error.value) {
+        message.value = ''
+      }
+      
     }
+
+  return { message, handleSubmit, error }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  form {
+    margin: 10px;
+  }
+  textarea {
+    width: 100%;
+    max-width: 100%;
+    margin-bottom: 6px;
+    padding: 10px;
+    box-sizing: border-box;
+    border: 0;
+    border-radius: 20px;
+    font-family: inherit;
+    outline: none;
+  }
 </style>
